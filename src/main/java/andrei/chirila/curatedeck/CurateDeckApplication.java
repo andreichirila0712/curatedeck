49e38eee93ffa836b544c4a1ff1287654d08594e
”Andrei” <andrei.chirila07121999@gmail.com>
2026-06-22 03:30:13 +0300

    chore: rename class

--- src/main/java/andrei/chirila/curatedeck/CurateDeckApplication.java
+++ src/main/java/andrei/chirila/curatedeck/CurateDeckApplication.java
@@ -2,14 +2,11 @@ package andrei.chirila.curatedeck;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-
 
 @SpringBootApplication
-@Controller
-public class ProveYourselfApplication {
+public class CurateDeckApplication {
 
 	static void main(String[] args) {
-		SpringApplication.run(ProveYourselfApplication.class, args);
+		SpringApplication.run(CurateDeckApplication.class, args);
 	}
 }
